bbbbfc454201428076ca2c41cb7ed7a207560336
Long <p.long351@gmail.com>
2025-08-30 15:49:32 +0700

    Changes for the xiao version

--- components/core/crazyflie/hal/src/ledseq.c
+++ components/core/crazyflie/hal/src/ledseq.c
@@ -52,7 +52,8 @@
 
 /* Led sequences */
 ledseqStep_t seq_lowbat_def[] = {
-  { true, LEDSEQ_WAITMS(1000)},
+  { true, LEDSEQ_WAITMS(100)},
+  { false, LEDSEQ_WAITMS(100)},
   {    0, LEDSEQ_LOOP},
 };
 
@@ -96,14 +97,14 @@ ledseqContext_t seq_calibrated = {
 };
 
 ledseqStep_t seq_alive_def[] = {
-  { true, LEDSEQ_WAITMS(50)},
-  {false, LEDSEQ_WAITMS(1950)},
+  { false, LEDSEQ_WAITMS(50)},
+  {true, LEDSEQ_WAITMS(1950)},
   {    0, LEDSEQ_LOOP},
 };
 
 ledseqContext_t seq_alive = {
   .sequence = seq_alive_def,
-  .led = SYS_LED,
+  .led = CHG_LED,
 };
 
 ledseqStep_t seq_linkup_def[] = {
